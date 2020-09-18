39437ed6418916f3607e784f4e572f2c434e72c3
asa-Q <asa.qu@qq.com>
2020-09-18 10:45:21 +0800

    update Coin runner

--- python/web3/brownie/samples/coin/scripts/Coin.py
+++ python/web3/brownie/samples/coin/scripts/Coin.py
@@ -4,4 +4,11 @@ from brownie import Coin, accounts
 
 
 def main():
-    return Coin.deploy({'from': accounts[0]})
+    coin = Coin.deploy({'from': accounts[0]})
+    print(coin.signatures)
+    tx = coin.setValue(5000)
+    print(tx.info())
+    vl = coin.getValue()
+    print(vl)
+    
+
