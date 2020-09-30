80ad66be388209ae6430306f01bcac1b4b5ba725
asa-Q <asa.qu@qq.com>
2020-09-30 10:29:36 +0800

    update swap test

--- nodejs/swap/migrations/1_initial_migration.js
+++ nodejs/swap/migrations/1_initial_migration.js
@@ -1,7 +1,15 @@
 const Migrations = artifacts.require("Migrations");
 const SushiToken = artifacts.require("SushiToken");
+const Timelock = artifacts.require("Timelock");
+//const ethers = artifacts.require("ethers");
+//const MockERC20 = artifacts.require("MockERC20");
+const MasterChef = artifacts.require("MasterChef");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(SushiToken);
+// deployer.deploy(Timelock);
+//  deployer.deploy(ethers);
+//  deployer.deploy(MockERC20);
+//  deployer.deploy(MasterChef);
 };
